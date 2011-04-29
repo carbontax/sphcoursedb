15b74e6398b6f95501df6104d3e58b08d199a1d1
carbontax@gmail.com <carbontax@gmail.com@dee2d0f3-b40b-2e70-2bb5-749109d7f33b>
2011-04-29 01:18:07 +0000

    initial import

---
+++ v1_50/com_sphcoursedb/admin/controller.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package    ca.utoronto.med.sph
+ * @subpackage Components
+ * @license    GNU/GPL
+ */
+
+// No direct access
+
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.application.component.controller');
+
+/**
+ * SPH Course DB Default Admin Controller
+ *
+ * @package    ca.utoronto.med.sph
+ * @subpackage Components
+ */
+class SPHCourseDBController extends JController
+{
+	/**
+	 * Method to display the view
+	 *
+	 * @access    public
+	 */
+	function display()
+	{
+		JRequest::setVar('view','courses');
+		parent::display();
+	}
+
+}
---
+++ v1_50/com_sphcoursedb/admin/controllers/courses.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package    ca.utoronto.med.sph
+ * @subpackage Components
+ * @license    GNU/GPL
+ */
+ 
+// No direct access
+ 
+defined( '_JEXEC' ) or die( 'Restricted access' );
+ 
+jimport('joomla.application.component.controller');
+ 
+/**
+ * SPH Course DB Default Admin Controller
+ *
+ * @package    ca.utoronto.med.sph
+ * @subpackage Components
+ */
+class SPHCourseDBControllerCourses extends JController
+{
+    /**
+     * Method to display the view
+     *
+     * @access    public
+     */
+    function display()
+    {
+        parent::display();
+    }
+ 
+}
