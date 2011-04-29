4296b8c796cce86ec4be13ef3e179393ac777211
carbontax@gmail.com <carbontax@gmail.com@dee2d0f3-b40b-2e70-2bb5-749109d7f33b>
2011-04-29 21:44:53 +0000

    RC1

--- trunk/v1_50/com_sphcoursedb/admin/install.sql
+++ trunk/v1_50/com_sphcoursedb/admin/install.sql
@@ -32,7 +32,7 @@ INSERT INTO `#__sphcoursedb_courses`
 `description`,`objectives`,`course_format`) 
 VALUES (1,"TEST Course 1","5555001","Course 1 Instructor","Course 1 prerequisites",
 "Course 1 description","Course 1 objectives", "Course 1 format"), 
-(2,"TEST Course 2","5555002","Course 2 Instructor","Course 2 prerequisites",
+(1,"TEST Course 2","5555002","Course 2 Instructor","Course 2 prerequisites",
 "Course 2 description","Course 2 objectives", "Course 2 format"), 
-(3,"TEST Course 3","5555003","Course 3 Instructor","Course 3 prerequisites",
+(1,"TEST Course 3","5555003","Course 3 Instructor","Course 3 prerequisites",
 "Course 3 description","Course 3 objectives", "Course 3 format");
--- trunk/v1_50/com_sphcoursedb/site/views/serieslist/tmpl/default.php
+++ trunk/v1_50/com_sphcoursedb/site/views/serieslist/tmpl/default.php
@@ -5,12 +5,13 @@ $k = 0;
 for ($i=0, $n=count( $this->items ); $i < $n; $i++)
 {
 	$row =& $this->items[$i];
-	$link = JRoute::_( 'index.php?option=com_sphcoursedb&controller=series&cid[]='. $row->id );
+	$link = JRoute::_( 'index.php?option=com_sphcoursedb&controller=series&cid='. $row->id );
 	?>
 	<div class="<?php echo "row$k"; ?>">
-		<div><a href="<?php echo $link; ?>"><?php echo $row->name; ?> </a>
-		</div>
-		<div><?php echo $row->description; ?>
+		<div>
+			<a href="<?php echo $link; ?>"><?php echo $row->name; ?> </a> 
+			<span>(<?php echo $row->description; ?>)
+			</span>
 		</div>
 	</div>
 	<?php
