3af16c7ddab7c34139a2a2dacd9fea48d74aeefd
carbontax@gmail.com <carbontax@gmail.com@dee2d0f3-b40b-2e70-2bb5-749109d7f33b>
2011-04-29 02:02:09 +0000

    adding more controllers and views to front end

--- v1_50/com_sphcoursedb/site/controller.php
+++ v1_50/com_sphcoursedb/site/controller.php
@@ -4,13 +4,13 @@
  * @subpackage Components
  * @license    GNU/GPL
  */
- 
+
 // No direct access
- 
+
 defined( '_JEXEC' ) or die( 'Restricted access' );
- 
+
 jimport('joomla.application.component.controller');
- 
+
 /**
  * SPH Course DB Component Controller
  *
@@ -19,15 +19,19 @@ jimport('joomla.application.component.controller');
  */
 class SPHCourseDBController extends JController
 {
-    /**
-     * Method to display the view
-     *
-     * @access    public
-     */
-    function display()
-    {
-    	JRequest::setVar('view','course');
-        parent::display();
-    }
- 
+	/**
+	 * Method to display the view
+	 *
+	 * @access    public
+	 */
+	function display()
+	{
+		$this->addModelPath(JPATH_COMPONENT_ADMINISTRATOR.DS.'models');
+		
+		if ( ! JRequest::getVar('view') ) {
+			JRequest::setVar('view','course');
+		}
+		parent::display();
+	}
+
 }
